e6cb9ca40a1e73e4537a230ac096276d19582723
Mateja <mateja.s2000@gmail.com>
2022-06-28 07:10:10 +0200

    Done for now

---
+++ src/Validator/CheckOutDateValidator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class CheckInDateValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) to take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        $checkInMinimum = date("H") > 3 ? date("Y-m-d", strtotime("+1 day")) : date("Y-m-d");
+        $checkInMaximum = date("H") > 3 ? date("Y-m-d", strtotime("+1 year -1 day")) : date("Y-m-d", strtotime("+1 year -2 day"));
+        $checkOutMinimum = date("H") > 3 ? date("Y-m-d", strtotime("+2 day")) : date("Y-m-d", strtotime("+1 day"));
+        $checkOutMaximum = date("H") > 3 ? date("Y-m-d", strtotime("+1 year")) : date("Y-m-d", strtotime("+1 year -1 day"));
+
+        $value =  date_format($value, 'Y-m-d');
+
+        if ($value < $checkInMinimum || $value > $checkInMaximum) {
+            // the argument must be a string or an object implementing __toString()
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}
